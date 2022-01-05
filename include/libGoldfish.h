59a8d234587691fe081cdff9934a7d44117dea33
Raffael Elias Schäfer <raffael1506@gmail.com>
2022-01-05 20:47:09 +0100

    toBe command

--- include/libGoldfish.h
+++ include/libGoldfish.h
@@ -10,16 +10,26 @@ private:
 protected:
 public:
     template<typename type>
-    static void equal(const std::string &, type, type);
+    [[maybe_unused]] static void toEqual(const std::string &, type, type);
+
+    [[maybe_unused]] static void toEqualString(const std::string &, const std::string &, const std::string &);
 
     template<typename type>
-    static void notEqual(const std::string &, type, type);
+    [[maybe_unused]] static void toNotEqual(const std::string &, type, type);
 
-    static float version();
+    [[maybe_unused]] static void toNotEqualString(const std::string &, const std::string &, const std::string &);
+
+    template<typename type>
+    [[maybe_unused]] static void toBe(const std::string &, type *, type *);
+
+    template<typename type>
+    [[maybe_unused]] static void toNotBe(const std::string &, type *, type *);
+
+    [[maybe_unused]] static float version();
 };
 
 template<typename type>
-void Goldfish::equal(const std::string &testName, type result, type expected) {
+[[maybe_unused]] void Goldfish::toEqual(const std::string &testName, type result, type expected) {
     if (result != expected) {
         std::string out = "Test failed!: " + testName + std::to_string(result) + " != " + std::to_string(expected);
         throw std::invalid_argument(out);
@@ -29,10 +39,36 @@ void Goldfish::equal(const std::string &testName, type result, type expected) {
 }
 
 template<typename type>
-void Goldfish::notEqual(const std::string &testName, type result, type expected) {
+[[maybe_unused]] void Goldfish::toNotEqual(const std::string &testName, type result, type expected) {
     if (result == expected) {
         std::string out = "Test failed!: " + testName + std::to_string(result) + " == " + std::to_string(expected);
         throw std::invalid_argument(out);
+    } else {
+        std::cout << "Test passed!: " << testName << result << " != " << expected << std::endl;
+    }
+}
+
+template<typename type>
+[[maybe_unused]] void Goldfish::toBe(const std::string &testName, type *result, type *expected) {
+    if (result != expected) {
+        if (*result == *expected) {
+            throw std::invalid_argument("Test failed!: The result is a duplicate of the expected value");
+        } else {
+            throw std::invalid_argument("Test failed!: The result is not the same a the expected value");
+        }
+    } else {
+        std::cout << "Test passed!: " << testName << result << " = " << expected << std::endl;
+    }
+}
+
+template<typename type>
+[[maybe_unused]] void Goldfish::toNotBe(const std::string &testName, type *result, type *expected) {
+    if (result != expected) {
+        if (*result == *expected) {
+            throw std::invalid_argument("Test failed!: The result is a duplicate of the expected value");
+        } else {
+            throw std::invalid_argument("Test failed!: The result is not the same a the expected value");
+        }
     } else {
         std::cout << "Test passed!: " << testName << result << " != " << expected << std::endl;
     }
--- libGoldfish.cpp
+++ libGoldfish.cpp
@@ -2,7 +2,25 @@
 #include <iostream>
 #include <string>
 
-float Goldfish::version() {
+[[maybe_unused]] void Goldfish::toEqualString(const std::string &testName, const std::string& result, const std::string& expected) {
+    if (result != expected) {
+        std::string out = "Test failed!: " + testName + result + " != " + expected;
+        throw std::invalid_argument(out);
+    } else {
+        std::cout << "Test passed!: " << testName << result << " = " << expected << std::endl;
+    }
+}
+
+[[maybe_unused]] void Goldfish::toNotEqualString(const std::string &testName, const std::string& result, const std::string& expected) {
+    if (result == expected) {
+        std::string out = "Test failed!: " + testName + result + " = " + expected;
+        throw std::invalid_argument(out);
+    } else {
+        std::cout << "Test passed!: " << testName << result << " != " << expected << std::endl;
+    }
+}
+
+[[maybe_unused]] float Goldfish::version() {
     std::cout << "Goldfish ><> Version 0.0.1\nMade with love by Raffael Elias Schaefer" << std::endl;
     return 0.01F;
 }
